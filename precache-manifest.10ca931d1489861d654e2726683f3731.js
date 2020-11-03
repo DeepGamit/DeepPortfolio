dcfdad2d70f816b4063174e1c0a9cef195c075dc
DeepGamit <deepkgamit@gmail.com>
2020-11-03 12:32:54 -0500

    Updates

---
+++ precache-manifest.10ca931d1489861d654e2726683f3731.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77ce8affa9d624de7374336ebea506a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ff2e2fb294b6dba7b869",
+    "url": "/portfolio/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "b9a6737e704a5406c023",
+    "url": "/portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "9c01caf4bf19dafcfa8b",
+    "url": "/portfolio/static/css/main.323febdc.chunk.css"
+  },
+  {
+    "revision": "a1dcbffc129a3e27a3d3",
+    "url": "/portfolio/static/js/2.47d02496.chunk.js"
+  },
+  {
+    "revision": "ff2e2fb294b6dba7b869",
+    "url": "/portfolio/static/js/3.9fa68f5f.chunk.js"
+  },
+  {
+    "revision": "b9a6737e704a5406c023",
+    "url": "/portfolio/static/js/4.15d1dada.chunk.js"
+  },
+  {
+    "revision": "9c01caf4bf19dafcfa8b",
+    "url": "/portfolio/static/js/main.d81350d3.chunk.js"
+  },
+  {
+    "revision": "1572f3051f4d8e57685c",
+    "url": "/portfolio/static/js/runtime-main.2434c13b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "fdc7d97e506f1122084b3d260066e177",
+    "url": "/portfolio/static/media/NodejsOriginalWordmark.fdc7d97e.svg"
+  },
+  {
+    "revision": "6260e6ed4600157055498824a340c1b4",
+    "url": "/portfolio/static/media/aws_cloud.6260e6ed.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "257721d63ac2396e7ef494b509df2600",
+    "url": "/portfolio/static/media/dhirubhai.257721d6.png"
+  },
+  {
+    "revision": "7fb59d3399dbff0f453a5761b5817d50",
+    "url": "/portfolio/static/media/jp_virtual.7fb59d33.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  }
+]);
